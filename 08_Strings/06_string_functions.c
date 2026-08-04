4855f1e835efe00d3da8d0509b5d0cd0dbbf5f49
Sourav Singh <souravsingh.rig@gmail.com>
2026-08-04 23:09:40 +0530

    Add files via upload

---
+++ 08_Strings/06_string_functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[] = "Sourav";
+    char s2[] = "bhai";
+    int a = strlen(str);
+    int b = strlen(s2);
+    printf("The length of str is %d\n",a);
+    printf("The length of s2 is %d\n",b);
+return 0;
+}
