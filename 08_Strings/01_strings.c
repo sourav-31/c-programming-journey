edf1f8ff4ca84899774bb15224b41e20ba99174c
Sourav Singh <souravsingh.rig@gmail.com>
2026-08-04 22:23:57 +0530

    Create 01_strings.c

---
+++ 08_Strings/01_strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char st[] = {'S','O','U','R','A','V','\0'};
+    char str[] = "SOURAV"; //same as above line
+    printf("The Second character is %c\n",st[1]);
+    
+    for(int i= 0; i < 6; i++){
+        printf("The Character is %c\n",str[i]);
+    }
+return 0;
+}
